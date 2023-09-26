0a193f2e69b19159fb998e7c812ce9b497f49d2f
edwar.lozano <edwar.lozano@mail.escuelaing.edu.co>
2023-09-25 20:39:31 -0500

    Funcioando, sin capturas de Docker

--- README.md
+++ README.md
@@ -1,43 +1,48 @@
-# Bono Parcial
+# Taller 5: TALLER DE DE MODULARIZACIÓN CON VIRTUALIZACIÓN E INTRODUCCIÓN A DOCKER
 **Estudiante:** Edwar Fabian Lozano Florez  
 **Clase:** AREP  
 **Profesor:** Luis Daniel Benavides
 
 ## Como funciona
-1. Primero se debe clonar el repositorio
-```
-   https://github.com/EdwarLozano/BonoParcial.git
-```
+1. Primero se debe clonar el repositorio: https://github.com/EdwarLozano/Taller5-AREP.git
 2. Luego se debe realizar el siguiente comando en la carpeta raíz
 ```
     mvn clean install
 ```
-3. Después el siguiente comando
-```
-    java -cp "target/classes;target/dependency/*" co.edu.escuelaing.arep.SparkWebServer
-```
+3. Se debe correr el método **Main** de la clase: src.main.java.co.edu.escuelaing.arep.**SparkWebServer.java**
 4. Desde un navegador acceder a:
-[web]: http://localhost:4567/hello
-5. Para acceder a las funcionalidades:
-- **Cos:**
-```
-    http://localhost:4567/cos/45
-```
-![img.png](img.png)
-- **Sin:**
-```
-    http://localhost:4567/sin/90
-```
-![img_1.png](img_1.png)
-- **Palíndromo:**
-```
-    http://localhost:4567/pal/oso
-```
-![img_2.png](img_2.png)
-- **Magnitud:**
-```
-    http://localhost:4567/mag/2/3
-```
-![img_3.png](img_3.png)
+   http://localhost:4567/funciones
+5. Para acceder a las funcionalidades simplemente se debe seleccionar la deseada e ingresar el o los valores solicitados
 
-**Nota:** Si se desea probar otro valor simplemente cambiandolo en el enlace  
+  ![img.png](img/img.png)
+
+## Pruebas de funciones:
+
+- **Sin:**
+
+  ![img_1.png](img/img_1.png)
+
+  ![img_2.png](img/img_2.png)
+- **Cos:**
+
+  ![img_3.png](img/img_3.png)
+
+  ![img_4.png](img/img_4.png)
+
+- **Palíndromo:**
+
+  ![img_5.png](img/img_5.png)
+
+  ![img_6.png](img/img_6.png)
+
+  **Nota:** Como se evidencia en la imágen la función funciona sin importar las mayúsculas.
+
+- **Magnitud:**
+
+  ![img_7.png](img/img_7.png)
+  
+  ![img_8.png](img/img_8.png)
+
+## Docker:
+
+
--- src/main/java/co/edu/escuelaing/arep/SparkWebServer.java
+++ src/main/java/co/edu/escuelaing/arep/SparkWebServer.java
@@ -1,35 +1,89 @@
 package co.edu.escuelaing.arep;
 
-import static spark.Spark.port;
-import static spark.Spark.get;
-import static java.lang.Math.*;
+import static spark.Spark.*;
 
 public class SparkWebServer {
-    public static void main(String... args){
+    public static void main(String... args) {
         port(getPort());
-        get("hello", (req,res) -> "Hello Docker!");
-        get("/sin/:valor", (req, res) -> {
-           double valor = Double.parseDouble(req.params(":valor"));
-           double solucion = Math.sin(valor);
-           return "Solución: " + solucion ;
+
+        get("/funciones", (req, res) -> {
+            return renderFormulario();
         });
 
-        get("/cos/:valor", (req, res) -> {
-            double valor = Double.parseDouble(req.params(":valor"));
-            double solucion = Math.cos(valor);
-            return "Solución: " + solucion ;
-        });
+        post("/calcular", (req, res) -> {
+            String funcion = req.queryParams("funcion");
+            String valorStr = req.queryParams("valor");
 
-        get("/pal/:valor", (req, res) -> {
-            String valor = req.params(":valor");
-            return "Solución: " + esPalindromo(valor) ;
-        });
 
-        get("/mag/:valorX/:valorY", (req, res) -> {
-            double valorX = Double.parseDouble(req.params(":valorX"));
-            double valorY = Double.parseDouble(req.params(":valorY"));
-            return "Solución: " + vectorMagnitude(valorX, valorY);
+            String resultado = "";
+            if ("sin".equals(funcion)) {
+                resultado = "Solución: " + Math.sin(Double.parseDouble(valorStr));
+            } else if ("cos".equals(funcion)) {
+                resultado = "Solución: " + Math.cos(Double.parseDouble(valorStr));
+            } else if ("pal".equals(funcion)) {
+                resultado = "Solución: " + esPalindromo(valorStr);
+            } else if ("mag".equals(funcion)) {
+                String valorYStr = req.queryParams("valorY");
+                resultado = "Solución: " + vectorMagnitude(Double.parseDouble(valorStr), Double.parseDouble(valorYStr));
+            }
+
+            return resultado;
         });
+    }
+
+    private static String renderFormulario() {
+        return "<html>" +
+                "<head>" +
+                "<title>Spark Formulario</title>" +
+                "</head>" +
+                "<body>" +
+                "<h1>Funciones</h1>" +
+                "<form action=\"/calcular\" method=\"post\">" +
+                "<label for=\"funcion\">Funcionalidad: </label>" +
+                "<select name=\"funcion\" id=\"funcion\">" +
+                "<option value=\"sin\">Sin</option>" +
+                "<option value=\"cos\">Cos</option>" +
+                "<option value=\"pal\">Palíndromo</option>" +
+                "<option value=\"mag\">Magnitud</option>" +
+                "</select>" +
+                "<br>" +
+                "<br>" +
+                "<div id=\"valores\">" +
+                "<label for=\"valor\">Valor: </label>" +
+                "<input type=\"text\" name=\"valor\" id=\"valor\">" +
+                "</div>" +
+                "<br>" +
+                "<input type=\"submit\" value=\"Calcular\">" +
+                "</form>" +
+                "<script>" +
+                "function mostrarCampos(funcionSeleccionada) {" +
+                "   var valoresDiv = document.getElementById(\"valores\");" +
+                "   valoresDiv.innerHTML = \"\";" +
+                "   var labelValor = document.createElement(\"label\");" +
+                "   labelValor.setAttribute(\"for\", \"valor\");" +
+                "   labelValor.textContent = \"Valor: \";" +
+                "   valoresDiv.appendChild(labelValor);" +
+                "   var inputValor = document.createElement(\"input\");" +
+                "   inputValor.type = \"text\";" +
+                "   inputValor.name = \"valor\";" +
+                "   inputValor.id = \"valor\";" +
+                "   valoresDiv.appendChild(inputValor);" +
+                "   if (funcionSeleccionada === \"mag\") {" +
+                "       valor.placeholder = \"Valor X\";" +
+                "       var inputX = document.createElement(\"input\");" +
+                "       inputX.type = \"text\";" +
+                "       inputX.name = \"valorY\";" +
+                "       inputX.placeholder = \"Valor Y\";" +
+                "       valoresDiv.appendChild(inputX);" +
+                "   }" +
+                "}" +
+                "var funcionSeleccionada = document.getElementById(\"funcion\");" +
+                "funcionSeleccionada.addEventListener(\"change\", function() {" +
+                "   mostrarCampos(this.value);" +
+                "});" +
+                "</script>" +
+                "</body>" +
+                "</html>";
     }
 
     private static int getPort() {
@@ -40,22 +94,20 @@ public class SparkWebServer {
         return 4567;
     }
 
-    public static boolean esPalindromo(String palabra) {
+    public static String esPalindromo(String palabra) {
         int i = 0;
         int j = palabra.length() - 1;
-        while (i <= j) {
-            if (palabra.charAt(i) != palabra.charAt(j)) {
-                return false;
+        while (i <= j && i < palabra.length() && j >= 0) {
+            if (palabra.toLowerCase().charAt(i) != palabra.toLowerCase().charAt(j)) {
+                return "La palabra " + palabra + " NO es palíndromo";
             }
             i++;
             j--;
         }
-        return true;
+        return "La palabra " + palabra + " SÍ es palíndromo";
     }
 
     public static double vectorMagnitude(double x, double y) {
         return Math.sqrt(x * x + y * y);
     }
-
 }
-
